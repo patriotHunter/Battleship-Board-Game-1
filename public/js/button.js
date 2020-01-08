29d6b948aea5c963aeec9c86a71ab38689968b93
pedroagrodrigues <pedroagrodrigues@gmail.com>
2020-01-08 00:46:40 +0000

    Fixed the background too

--- public/js/button.js
+++ public/js/button.js
@@ -1,3 +1,12 @@
+var darkMode = false;
+
 function modeFunction() {
-	$('div.banner').toggleClass('banner-dark-mode');
+	$('div.banner').toggleClass('dark-mode');
+	$('div.content').toggleClass('dark-mode');
+	if (darkMode) 
+	$('body').css({ 'background-color': '#636b6f' });
+	else 
+	$('body').css({ 'background-color': '#f5f5f7' });
+
+	darkMode = !darkMode;
 }
